e1813432cc17226a2509e952462dab2f8666ca3e
xinyujiang <jiangxinyu@benewake.com>
2017-05-25 16:10:08 +0800

    add deps_tools

---
+++ deps_tool/resolve_deps.sh
@@ -0,0 +1,16 @@
+#! /bin/bash -
+
+# Get package path
+THIS_SCRIPT=`readlink -f $0`
+THIS_SCRIPT_DIR=`dirname $THIS_SCRIPT`
+THIS_PKG=`dirname $THIS_SCRIPT_DIR`
+WS_SRC_DIR=`dirname $THIS_PKG`
+WS_DIR=`dirname $WS_SRC_DIR`
+
+source $WS_DIR/installer/config.bash
+source $WS_SRC_DIR/bwslam_meta/deps_tool/dep_tools.sh
+
+# Identify package name and version
+identify_package $THIS_PKG
+
+#################### Add your scripts below  ####################
